c54420015426412beaabf31b076e86ca4bf801a3
ubednama <shahidkhan.ak18@gmail.com>
2024-09-24 04:04:00 +0530

    add SelectionSort.java

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,2 +1,51 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class SelectionSort {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int tc = 0; tc < t; tc++) {
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            selectionSort(arr);
+            System.out.println(Arrays.toString(arr));
+        }
+
+        sc.close();
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int last) {
+        int max = start;
+        for (int i = start; i <= last; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
 
